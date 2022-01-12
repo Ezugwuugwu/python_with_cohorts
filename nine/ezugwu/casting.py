1a6032c5f0504705133eef88e5e03eb762060621
Ezugwu Joel <ezugwuugwu@gmail.com>
2022-01-12 17:20:38 +0100

    commit Assignment

---
+++ nine/ezugwu/casting.py
@@ -0,0 +1,28 @@
+first_name = "Johnnie"
+last_name = "Doe"
+age = 47
+state_of_origin = "Lagos"
+
+for i in range(0,len(first_name),2):
+    print(first_name[i].upper(), age // 10, sep = "",end= "")
+
+print("\n")
+for i in range(0, len(state_of_origin), 2):
+        print(state_of_origin[i].upper(), last_name.upper(), sep= "", end= "")
+
+print("\n")
+# for i in range(0, len(first_name),1):
+#     print(first_name[i].upper(), sep= "", end= "-")
+
+# for i in range(0, len(last_name), 1):
+#         print(last_name[i].upper(), sep="", end="-")
+
+print()
+full_name = first_name+last_name
+full_name = full_name.upper()
+for i in range(len(full_name)):
+    if i == len(full_name)-1:
+        print(full_name[i])
+    else:
+        print(full_name[i], end= "-")
+print()
